d480c4def411ef5fab91c7caa9b6a1bfb6b69574
William Dewes <william.dewes@icloud.com>
2023-03-24 08:41:29 -0300

    finalizing creation of pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,52 @@
 package application;
+import chess.ChessException;
+import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
-import boardgame.Position;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ChessMatch chessMatch = new ChessMatch();
+        List<ChessPiece> captured = new ArrayList<>();
+
+        while (!chessMatch.getCheckMate()){
+           try{
+               UI.clearScreen();
+               UI.printMatch(chessMatch, captured);
+               System.out.println();
+               System.out.print("Source: ");
+               ChessPosition source = UI.readChessPosition(scanner);
+
+               boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+               UI.clearScreen();
+               UI.printBoard(chessMatch.getPieces(), possibleMoves);
+
+               System.out.println();
+               System.out.print("Target: ");
+               ChessPosition target = UI.readChessPosition(scanner);
+
+               ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+               if(capturedPiece != null){
+                   captured.add(capturedPiece);
+               }
+           }
+           catch (ChessException ex){
+               System.out.println(ex.getMessage());
+               scanner.nextLine();
+            }
+           catch(InputMismatchException ex){
+               System.out.println(ex.getMessage());
+               scanner.nextLine();
+           }
+        }
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
